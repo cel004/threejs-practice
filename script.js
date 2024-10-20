40b39340c0eb9db9afaa5923d40afbf40580637f
cel004 <ngcelinaa@gmail.com>
2024-10-20 19:20:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,66 +4,6 @@ import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/js
 // allows imports of .gltf files
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
-let lightMode = true;
-const asciiMesh = new THREE.Mesh();
-const scene = new THREE.Scene();
-scene.background = new THREE.Color(0, 0, 0);
-
-const pointLight1 = new THREE.PointLight(0xffffff, 1);
-pointLight1.position.set(100, 100, 400);
-scene.add(pointLight1);
-
-const pointLight2 = new THREE.PointLight(0xffffff, 0.5);
-pointLight2.position.set(-500, 100, -400);
-scene.add(pointLight2);
-
-const material = new THREE.MeshStandardMaterial({
-    flatShading: true,
-    side: THREE.DoubleSide,
-});
-
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight,
-};
-
-const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 2000);
-camera.position.set(0, 100, 400);
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(sizes.width, sizes.height);
-document.body.appendChild(renderer.domElement);
-
-// ascii shader effect
-let effect;
-const characters = ' .:-+*=%@#';
-const effectSize = { amount: 0.205 };
-let backgroundColor = 'black';
-let ASCIIColor = 'white';
-
-function createEffect() {
-    effect = new AsciiEffect(renderer, characters, { invert: true, resolution: effectSize.amount });
-    effect.setSize(sizes.width, sizes.height);
-    effect.domElement.style.color = ASCIIColor;
-    effect.domElement.style.backgroundColor = backgroundColor;
-}
-
-createEffect();
-document.body.appendChild(effect.domElement);
-document.getElementById('ascii').style.whiteSpace = 'pre-wrap';
-
-// gltb Loader for 3d object
-const gltbLoader = new GLTBLoader();
-
-stlLoader.load(
-    './assets/ChocoCat.glb',
-    function (geometry) {
-        asciiMesh.material = material;
-        asciiMesh.geometry = geometry;
-
-        geometry.computeVertexNormals();
-        asciiMesh.geometry.center();
-        asciiMesh.rotation.x = -Math.PI / 2; // upright rotation
 
 // intialize scene
 const scene = new THREE.Scene();
